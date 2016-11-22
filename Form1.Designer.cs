5d891471d4491ac41bfb0f2e0cac2c069fd02b46
Lidia <Lidia>
2016-11-22 22:42:52 +0300

    Form1 os in original

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,11 +29,15 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabInput = new System.Windows.Forms.TabPage();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.progressBarCurrentCount = new System.Windows.Forms.ProgressBar();
+            this.progressBarTotal = new System.Windows.Forms.ProgressBar();
             this.buttonLaunch = new System.Windows.Forms.Button();
             this.groupBoxGeneration = new System.Windows.Forms.GroupBox();
             this.numericUpDownVerticesStep = new System.Windows.Forms.NumericUpDown();
@@ -43,20 +47,26 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.groupBoxAdvanced = new System.Windows.Forms.GroupBox();
+            this.textBoxRadiusMax = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBoxRadiusMin = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.numericUpDownIterationsCount = new System.Windows.Forms.NumericUpDown();
             this.label7 = new System.Windows.Forms.Label();
             this.groupBoxBasic = new System.Windows.Forms.GroupBox();
             this.label5 = new System.Windows.Forms.Label();
+            this.radioButton3D = new System.Windows.Forms.RadioButton();
+            this.radioButton2D = new System.Windows.Forms.RadioButton();
             this.numericUpDownRadius = new System.Windows.Forms.NumericUpDown();
             this.label1 = new System.Windows.Forms.Label();
             this.tabResults = new System.Windows.Forms.TabPage();
             this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.textBoxTotalTime = new System.Windows.Forms.TextBox();
-            this.textBoxAvgRadius = new System.Windows.Forms.TextBox();
             this.label11 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
             this.toolTipTestWarning = new System.Windows.Forms.ToolTip(this.components);
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBoxAvgRadius = new System.Windows.Forms.TextBox();
             this.tabControl1.SuspendLayout();
             this.tabInput.SuspendLayout();
             this.groupBoxGeneration.SuspendLayout();
@@ -85,6 +95,10 @@
             // 
             // tabInput
             // 
+            this.tabInput.Controls.Add(this.label13);
+            this.tabInput.Controls.Add(this.label12);
+            this.tabInput.Controls.Add(this.progressBarCurrentCount);
+            this.tabInput.Controls.Add(this.progressBarTotal);
             this.tabInput.Controls.Add(this.buttonLaunch);
             this.tabInput.Controls.Add(this.groupBoxGeneration);
             this.tabInput.Controls.Add(this.groupBoxAdvanced);
@@ -98,10 +112,46 @@
             this.tabInput.Text = "Исходные данные";
             this.tabInput.UseVisualStyleBackColor = true;
             // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(9, 352);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(279, 20);
+            this.label13.TabIndex = 13;
+            this.label13.Text = "Прогресс для текущего кол-ва вершин";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(9, 321);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(127, 20);
+            this.label12.TabIndex = 12;
+            this.label12.Text = "Общий прогресс";
+            // 
+            // progressBarCurrentCount
+            // 
+            this.progressBarCurrentCount.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBarCurrentCount.Location = new System.Drawing.Point(308, 347);
+            this.progressBarCurrentCount.Name = "progressBarCurrentCount";
+            this.progressBarCurrentCount.Size = new System.Drawing.Size(266, 25);
+            this.progressBarCurrentCount.TabIndex = 12;
+            // 
+            // progressBarTotal
+            // 
+            this.progressBarTotal.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBarTotal.Location = new System.Drawing.Point(308, 316);
+            this.progressBarTotal.Name = "progressBarTotal";
+            this.progressBarTotal.Size = new System.Drawing.Size(266, 25);
+            this.progressBarTotal.TabIndex = 11;
+            // 
             // buttonLaunch
             // 
-            this.buttonLaunch.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonLaunch.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonLaunch.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.buttonLaunch.Location = new System.Drawing.Point(0, 403);
             this.buttonLaunch.Name = "buttonLaunch";
@@ -113,8 +163,8 @@
             // 
             // groupBoxGeneration
             // 
-            this.groupBoxGeneration.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxGeneration.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBoxGeneration.Controls.Add(this.numericUpDownVerticesStep);
             this.groupBoxGeneration.Controls.Add(this.label2);
             this.groupBoxGeneration.Controls.Add(this.numericUpDownVerticesMax);
@@ -224,9 +274,13 @@
             // 
             // groupBoxAdvanced
             // 
-            this.groupBoxAdvanced.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxAdvanced.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxAdvanced.Controls.Add(this.textBoxRadiusMax);
+            this.groupBoxAdvanced.Controls.Add(this.label9);
+            this.groupBoxAdvanced.Controls.Add(this.textBoxRadiusMin);
+            this.groupBoxAdvanced.Controls.Add(this.label8);
             this.groupBoxAdvanced.Controls.Add(this.label6);
             this.groupBoxAdvanced.Controls.Add(this.numericUpDownIterationsCount);
             this.groupBoxAdvanced.Controls.Add(this.label7);
@@ -238,14 +292,48 @@
             this.groupBoxAdvanced.TabStop = false;
             this.groupBoxAdvanced.Text = "Дополнительные параметры";
             // 
+            // textBoxRadiusMax
+            // 
+            this.textBoxRadiusMax.Location = new System.Drawing.Point(424, 123);
+            this.textBoxRadiusMax.Name = "textBoxRadiusMax";
+            this.textBoxRadiusMax.Size = new System.Drawing.Size(131, 27);
+            this.textBoxRadiusMax.TabIndex = 11;
+            this.textBoxRadiusMax.Text = "4";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(394, 126);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(26, 20);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "до";
+            // 
+            // textBoxRadiusMin
+            // 
+            this.textBoxRadiusMin.Location = new System.Drawing.Point(424, 90);
+            this.textBoxRadiusMin.Name = "textBoxRadiusMin";
+            this.textBoxRadiusMin.Size = new System.Drawing.Size(131, 27);
+            this.textBoxRadiusMin.TabIndex = 9;
+            this.textBoxRadiusMin.Text = "2";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(394, 93);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(24, 20);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "от";
+            // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Location = new System.Drawing.Point(5, 113);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(156, 20);
+            this.label6.Size = new System.Drawing.Size(373, 20);
             this.label6.TabIndex = 8;
-            this.label6.Text = "Радиус связности = 1";
+            this.label6.Text = "Радиус покрытия одной вершины лежит в границах";
             // 
             // numericUpDownIterationsCount
             // 
@@ -281,6 +369,8 @@
             // groupBoxBasic
             // 
             this.groupBoxBasic.Controls.Add(this.label5);
+            this.groupBoxBasic.Controls.Add(this.radioButton3D);
+            this.groupBoxBasic.Controls.Add(this.radioButton2D);
             this.groupBoxBasic.Controls.Add(this.numericUpDownRadius);
             this.groupBoxBasic.Controls.Add(this.label1);
             this.groupBoxBasic.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -299,6 +389,29 @@
             this.label5.Size = new System.Drawing.Size(98, 20);
             this.label5.TabIndex = 4;
             this.label5.Text = "Тип области:";
+            // 
+            // radioButton3D
+            // 
+            this.radioButton3D.AutoSize = true;
+            this.radioButton3D.Location = new System.Drawing.Point(148, 99);
+            this.radioButton3D.Name = "radioButton3D";
+            this.radioButton3D.Size = new System.Drawing.Size(71, 24);
+            this.radioButton3D.TabIndex = 3;
+            this.radioButton3D.TabStop = true;
+            this.radioButton3D.Text = "Сфера";
+            this.radioButton3D.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2D
+            // 
+            this.radioButton2D.AutoSize = true;
+            this.radioButton2D.Checked = true;
+            this.radioButton2D.Location = new System.Drawing.Point(148, 69);
+            this.radioButton2D.Name = "radioButton2D";
+            this.radioButton2D.Size = new System.Drawing.Size(58, 24);
+            this.radioButton2D.TabIndex = 2;
+            this.radioButton2D.TabStop = true;
+            this.radioButton2D.Text = "Круг";
+            this.radioButton2D.UseVisualStyleBackColor = true;
             // 
             // numericUpDownRadius
             // 
@@ -344,19 +457,19 @@
             // 
             // chart
             // 
-            this.chart.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            chartArea1.Name = "ChartArea1";
-            this.chart.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart.Legends.Add(legend1);
+            this.chart.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea4.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart.Legends.Add(legend4);
             this.chart.Location = new System.Drawing.Point(7, 41);
             this.chart.Name = "chart";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chart.Series.Add(series1);
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart.Series.Add(series4);
             this.chart.Size = new System.Drawing.Size(567, 405);
             this.chart.TabIndex = 4;
             this.chart.Text = "chart1";
@@ -369,14 +482,6 @@
             this.textBoxTotalTime.Size = new System.Drawing.Size(100, 27);
             this.textBoxTotalTime.TabIndex = 3;
             // 
-            // textBoxAvgRadius
-            // 
-            this.textBoxAvgRadius.Location = new System.Drawing.Point(471, 8);
-            this.textBoxAvgRadius.Name = "textBoxAvgRadius";
-            this.textBoxAvgRadius.ReadOnly = true;
-            this.textBoxAvgRadius.Size = new System.Drawing.Size(100, 27);
-            this.textBoxAvgRadius.TabIndex = 2;
-            // 
             // label11
             // 
             this.label11.AutoSize = true;
@@ -385,6 +490,11 @@
             this.label11.Size = new System.Drawing.Size(113, 20);
             this.label11.TabIndex = 1;
             this.label11.Text = "Время работы:";
+            // 
+            // toolTipTestWarning
+            // 
+            this.toolTipTestWarning.ToolTipTitle = "Перед началом тестирования убедитесь, что у вас введены значения минимума и макси" +
+                "мума радиуса покрытия вершин.";
             // 
             // label10
             // 
@@ -395,25 +505,27 @@
             this.label10.TabIndex = 0;
             this.label10.Text = " ";
             // 
-            // toolTipTestWarning
+            // textBoxAvgRadius
             // 
-            this.toolTipTestWarning.ToolTipTitle = "Перед началом тестирования убедитесь, что у вас введены значения минимума и макси" +
-    "мума радиуса покрытия вершин.";
+            this.textBoxAvgRadius.Location = new System.Drawing.Point(471, 8);
+            this.textBoxAvgRadius.Name = "textBoxAvgRadius";
+            this.textBoxAvgRadius.ReadOnly = true;
+            this.textBoxAvgRadius.Size = new System.Drawing.Size(100, 27);
+            this.textBoxAvgRadius.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.LightCoral;
             this.ClientSize = new System.Drawing.Size(582, 484);
             this.Controls.Add(this.tabControl1);
             this.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.tabControl1.ResumeLayout(false);
             this.tabInput.ResumeLayout(false);
+            this.tabInput.PerformLayout();
             this.groupBoxGeneration.ResumeLayout(false);
             this.groupBoxGeneration.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownVerticesStep)).EndInit();
@@ -448,15 +560,25 @@
         private System.Windows.Forms.GroupBox groupBoxAdvanced;
         private System.Windows.Forms.GroupBox groupBoxBasic;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton radioButton3D;
+        private System.Windows.Forms.RadioButton radioButton2D;
         private System.Windows.Forms.NumericUpDown numericUpDownRadius;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.NumericUpDown numericUpDownIterationsCount;
+        private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxRadiusMin;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBoxRadiusMax;
         private System.Windows.Forms.ToolTip toolTipTestWarning;
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.TextBox textBoxTotalTime;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+        private System.Windows.Forms.ProgressBar progressBarTotal;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.ProgressBar progressBarCurrentCount;
+        private System.Windows.Forms.Label label13;
         private System.Windows.Forms.TextBox textBoxAvgRadius;
         private System.Windows.Forms.Label label10;
     }
